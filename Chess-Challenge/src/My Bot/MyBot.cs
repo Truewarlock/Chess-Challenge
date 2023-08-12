b2baaaae3639012b018db66dd8af54b2ca6bc4b2
Truewarlock <valytruewarlock@gmail.com>
2023-08-12 15:54:58 +0300

    can detect checkmate

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,32 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+
+        Random random = new Random();
+
+        Move theTwoHundreddIqMove = moves[random.Next(moves.Length)];
+
+        foreach (var move in moves)
+        {
+            if (CheckIfCanCheckMate(board, move)){
+                theTwoHundreddIqMove = move;
+                break;
+            }
+        }
+        return theTwoHundreddIqMove;
+    }
+
+
+    private bool CheckIfCanCheckMate(Board board, Move move) {
+        board.MakeMove(move);
+        bool ggwp = board.IsInCheckmate();
+        board.UndoMove(move);
+        return ggwp;
+
     }
 }
